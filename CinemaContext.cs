4b3d0927634ba4766fbb8630b6d55fdc32c4b2b4
Artikman <47893341+Artikman@users.noreply.github.com>
2019-05-08 21:22:05 +0300

    Add files via upload

---
+++ CinemaContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab6.Models;
+
+namespace Lab6
+{
+    public class CinemaContext : DbContext
+    {
+
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Place> Places { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Lab6.Services;
+using Lab6.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lab6
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CinemaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
+            services.AddTransient<DbService>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseDbInitMiddleware();
+            app.UseMvc();
+        }
+    }
+}
