6f74fdf3a4dfc0759ec9a0538ff1f89830adf565
Artikman <47893341+Artikman@users.noreply.github.com>
2019-05-08 21:19:36 +0300

    Add files via upload

---
+++ Controllers/GsmsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Lab6.Models;
+using Lab6.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab6.Controllers
+{
+    [Route("api/[controller]")]
+    public class GsmsController : Controller
+    {
+        private DbService dbService;
+
+        public GsmsController(DbService service)
+        {
+            dbService = service;
+        }
+
+        //GET api/gsms
+        [HttpGet]
+        public IEnumerable<PrepareCinema> Get()
+        {
+            return dbService.ListCinema();
+        }
+
+        //GET api/gsms
+        [HttpGet("{id}")]
+        public List<Genre> Get(int id)
+        {
+            return dbService.FindCinemaById(id);
+        }
+
+        //POST api/gsms
+        [HttpPost]
+        public void Post([FromBody]GenreBody value)
+        {
+            dbService.PostCinema(value);
+        }
+
+        //PUT api/gsms
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]UpdateModel value)
+        {
+            dbService.UpdateCinema(id, value);
+        }
+
+        //DELETE api/gsms
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dbService.DeleteCinema(id);
+        }
+    }
+}   
